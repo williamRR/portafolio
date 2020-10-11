719babf8ef2c7e162c6765a4d90c0ab6414ed347
William <william.rehel@gmail.com>
2020-10-11 13:00:15 -0300

    Updates

---
+++ precache-manifest.e012663c216d2363030fd417a9b7b06c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f24cd4382a2eada5578270aa126a8d",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "e29f01d6c56e03bb97ef",
+    "url": "/portafolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7f56c2a9170ca5c2372d",
+    "url": "/portafolio/static/css/main.dc06ea7d.chunk.css"
+  },
+  {
+    "revision": "e29f01d6c56e03bb97ef",
+    "url": "/portafolio/static/js/2.0c201ed1.chunk.js"
+  },
+  {
+    "revision": "451ffe720aa8add9f554b8ed54913322",
+    "url": "/portafolio/static/js/2.0c201ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f56c2a9170ca5c2372d",
+    "url": "/portafolio/static/js/main.ed747742.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  },
+  {
+    "revision": "067997109f5ac39f8848f62fe92a24c0",
+    "url": "/portafolio/static/media/logo.06799710.png"
+  }
+]);
